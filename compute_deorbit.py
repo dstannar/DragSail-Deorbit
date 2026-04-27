436b4d04d0749386c0f936d8c362c8848d266fbe
Kyriacos <kmphilip@calpoly.edu>
2026-04-26 17:51:26 -0700

    Update compute_deorbit.py
    Attempted to add plotting of deorbit over time

--- compute_deorbit.py
+++ compute_deorbit.py
@@ -1,9 +1,11 @@
 # compute deorbit
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def compute_deorbit(
     t,
+    delta_t,
     state,
     muE=398600,
     Re_km=6378,
@@ -22,56 +24,81 @@ def compute_deorbit(
     ----------
     t : float
         Time [s]
+    delta_t : float
+        Time Step [s]
     state : ndarray, shape (6,)
-        [x, y, z, vx, vy, vz] in km and km/s
+        [x, y, z, vx, vy, vz] in [km] and [km/s]
         
 
     Returns
     -------
     dstate : ndarray, shape (6,)
-        [vx, vy, vz, ax, ay, az]
+        [vx, vy, vz, ax, ay, az] in [km/s] and [km/s^2] (probably this needs to be confirmed)
     """
-    state = np.asarray(state, dtype=float).reshape(6,)
+    
+     state = np.asarray(state, dtype=float).reshape(6,)
     r = state[:3]
     v = state[3:]
 
     rmag = np.linalg.norm(r)
+    
+    t_list = [t]
+    alt_list = [rmag]
+    t0 = t
 
-    # Two-body gravity
-    a_tb = -muE * r / rmag**3
+    while (rmag > Re_km):
 
-    # Drag
-    a_drag = drag_acceleration(
-        r_eci_km=r,
-        v_eci_km_s=v,
-        Cd=Cd,
-        area_m2=area_m2,
-        mass_kg=mass_kg,
-        Re_km=Re_km,
-        omegaE_rad_s=omegaE_rad_s,
-    )
-
-    # J2
-    a_j2 = j2_acceleration(
-        r=r,
-        muE=muE,
-        Re=Re_km,
-        J2=J2,
-    )
-
-    # SRP (optional for now)
-    if r_sun_km is None:
-        a_srp = np.zeros(3)
-    else:
-        a_srp = srp_acceleration(
-            r_sc=r,
-            r_sun=r_sun_km,
-            mass=mass_kg,
+        # Two-body gravity
+        a_tb = -muE * r / rmag**3
+    
+        # Drag
+        a_drag = drag_acceleration(
+            r_eci_km=r,
+            v_eci_km_s=v,
+            Cd=Cd,
             area_m2=area_m2,
-            Cr=Cr,
-            Re=Re_km,
+            mass_kg=mass_kg,
+            Re_km=Re_km,
+            omegaE_rad_s=omegaE_rad_s,
         )
+    
+        # J2
+        a_j2 = j2_acceleration(
+            r=r,
+            muE=muE,
+            Re=Re_km,
+            J2=J2,
+        )
+    
+        # SRP (optional for now)
+        if r_sun_km is None:
+            a_srp = np.zeros(3)
+        else:
+            a_srp = srp_acceleration(
+                r_sc=r,
+                r_sun=r_sun_km,
+                mass=mass_kg,
+                area_m2=area_m2,
+                Cr=Cr,
+                Re=Re_km,
+            )
+    
+        a_total = a_tb + a_drag + a_j2 + a_srp
 
-    a_total = a_tb + a_drag + a_j2 + a_srp
+        r += v*delta_t + 0.5*a_total*(delta_t**2)
+        rmag = np.linalg.norm(r)
+        v += a_total*delta_t
 
-    return np.hstack((v, a_total))
+        t += delta_t
+        t_list.append(t)
+        alt_list.append(rmag)
+
+    plt.plot(t_list, alt_list)
+
+    plt.title("Deorbit Simulation")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Altitude [km]")
+
+    plt.show()
+
+    return t - t0
