d81675eb8c99e975fc71e2fbcb2f172dc3a1ee10
Kyriacos <kmphilip@calpoly.edu>
2026-04-29 22:25:04 -0700

    Update j2_model.py
    Improved documentation

--- j2_model.py
+++ j2_model.py
@@ -4,11 +4,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def j2_acceleration(
-          r,
-          muE,
-          Re,
-          J2,
-):
+          r: np.ndarray,
+          muE: float,
+          Re: float,
+          J2: float,
+) -> float:
+    """
+    Compute J2 Perturbation acceleration.
+
+    Parameters
+    ----------
+    r : ndarray (3,)
+        Spacecraft position vector in ECI [km]
+
+    muE : float
+        The Earth's gravitational parameter [km^3/s^2]
+
+    Re : float
+        Radius of the Earth [km]
+
+    J2 : float
+        J2 constant
+
+    Returns
+    -------
+    a_j2 : ndarray (3,)
+        J2 acceleration vector [km/s^2]
+    """
 
     rmag = np.linalg.norm(r)
     x = r[0]
